b8eb5300a04defbedaf7b3e0725dc4048f81a4f0
excessraccoon <88817155+excessraccoon@users.noreply.github.com>
2021-08-27 19:55:07 +0530

    version 2

---
+++ js/js/JScript.js
@@ -0,0 +1,18 @@
+function acceptTems()
+{
+	var chk = document.getElementById("chkTerms");
+	var sub = document.getElementById("btnSub");
+	
+	if (chk.checked)
+	{
+		sub.disabled =false;
+		
+	}
+	else	
+	{
+	 sub.disabled=true;	 
+	}
+	
+}
+		
+	
